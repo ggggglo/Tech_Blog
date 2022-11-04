9002892312319f8c703386aaf93fa5b716475dca
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-11-04 18:09:03 -0500

    added format date helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,10 @@
+function formatDate(date) {
+  return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+}
+
+
 module.exports = {
-  format_time: (date) => {
-    return date.toLocaleTimeString();
-  },
-  format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
-    }`;
-  },
-};
+  formatDate
+}
